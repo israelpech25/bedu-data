ea5d7f7bffbadaef00dedb34f2b812b9a8a310f6
israelpech25 <israelhernandezpech@gmail.com>
2020-11-21 13:25:42 -0600

    Added file to explain lists in python

---
+++ 01_listas.py
@@ -0,0 +1,15 @@
+#archivo explicando listas
+
+numeros = [23, 89, 127, 1289, 8912]
+print(numeros)
+
+#para agregar un elemnto a una lista usamos appendnumerp_extra = 33
+
+numero_extra = 33
+numero_extra_dos = 1233
+numeros.append(numero_extra)
+
+print(numeros)
+
+numeros.pop()
+print(numeros)
