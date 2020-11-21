509c37d066e3e10b19418aa76c8bbbc8b33847fe
israelpech25 <israelhernandezpech@gmail.com>
2020-11-21 14:33:20 -0600

    bucle infinito

---
+++ 05_lista_aleatoria.py
@@ -0,0 +1,12 @@
+# Generar una lista con valores aleatorios
+
+from random import randint
+
+elementos = input('Cuantos elementos necesitas?')
+elementos = int(elementos)
+
+contador = 1
+
+while contador <= elementos:
+    print('Contador es menor')
+    contador = contador + 1
