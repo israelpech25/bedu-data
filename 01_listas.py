68adbd949aebbd7d9f419bb96d5ca001797b4670
israelpech25 <israelhernandezpech@gmail.com>
2021-02-09 22:09:54 -0600

    Ejercicio append y pop

--- 01_listas.py
+++ 01_listas.py
@@ -1,15 +1,19 @@
-#archivo explicando listas
+# Archivo explicando listas
 
 numeros = [23, 89, 127, 1289, 8912]
 print(numeros)
 
-#para agregar un elemnto a una lista usamos appendnumerp_extra = 33
+#para agregar un elemnto a una lista usamos append
 
 numero_extra = 33
 numero_extra_dos = 1233
-numeros.append(numero_extra)
+numero_extra_tres = 89
 
+numeros.append(numero_extra)
+ 
 print(numeros)
 
+#Para eliminar el último elemento de una lista usamos pop
 numeros.pop()
+
 print(numeros)
